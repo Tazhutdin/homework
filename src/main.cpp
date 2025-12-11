db647072258f41feaa19e204cecb2369d70a6694
Tazhutdin <tazhutdind@mail.ru>
2025-12-11 22:30:11 +0300

    Initial project structure

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Library.h"  // или другие нужные заголовки
+
+int main() {
+    std::cout << "Library Management System\n";
+
+    // Ваш код здесь
+
+    return 0;
+}
